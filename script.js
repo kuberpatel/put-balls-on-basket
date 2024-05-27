2ec21179e942eb76f749894c3cca6061838d092f
Kuber Patel <101393723+kuberpatel@users.noreply.github.com>
2024-05-27 18:54:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,27 @@
-$(document).ready(function () {
-  const ballColors = [
-    "red",
-    "blue",
-    "yellow",
-    "lightgrey",
-    "pink",
-    "black",
-    "orange",
-    "green",
-    "purple",
-    "magenta",
-    "firebrick",
-    "crimson",
-  ];
+var colors = [
+  "red",
+  "blue",
+  "yellow",
+  "lightgrey",
+  "darkorchid",
+  "black",
+  "orange",
+  "deeppink",
+  "green",
+  "purple",
+  "saddlebrown",
+  "lightseagreen",
+  "deepskyblue",
+  "firebrick",
+  "crimson",
+];
 
-  const basketWidth = $("#basket").width();
-  const basketHeight = $("#basket").height();
-  const ballSize = 60; //ball size(50px) + margin (5px on each side )
-  const maxBallsPerRow = Math.floor(basketWidth / ballSize);
-  const maxRows = Math.floor(basketHeight / ballSize);
-  const maxBalls = maxBallsPerRow * maxRows;
+var box = $("#box");
 
-  $("#start").click(function () {
-    $("#basket").empty(); //Clear the basket before stacking new balls
-
-    //ball stacking process
-
-    for (let i = 0; i < maxBalls; i++) {
-      const randomColor =
-        ballColors[Math.floor(Math.random() * ballColors.length)];
-      const newBall = $(
-        '<div class="ball" style="background-color: ' +
-          randomColor +
-          ' "></div>'
-      );
-      $("#basket").append(newBall); // Append the new ball to the basket
-    }
-  });
+$("button").click(function (e) {
+  box.append(
+    '<div class="ball" style="background-color: ' +
+      colors[Math.floor(Math.random() * 15)] +
+      '"></div>'
+  );
 });
